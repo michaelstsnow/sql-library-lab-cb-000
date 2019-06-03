adc0ddfd0cc55e54228f1206acf82155807f2831
michaelstsnow <michael.st.snow@gmail.com>
2019-06-03 00:29:13 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,11 @@
+INSERT INTO series (id, title, author_id, subgenre_id)
+
+INSERT INTO subgenres (id, name)
+
+INSERT INTO authors (id, name)
+
+INSERT INTO books (id, title, year, series_id)
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id)
+
+INSERT INTO character_books (id, book_id, character_id) 
