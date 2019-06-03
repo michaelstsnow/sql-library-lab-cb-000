495474007d3205a068166e4389e730d1565bb550
michaelstsnow <michael.st.snow@gmail.com>
2019-06-03 00:30:09 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,11 +1,16 @@
-CREATE TABLE series (id, title, author_id, subgenre_id)
+CREATE TABLE series
+  (id,
+    title,
+    author_id,
+    subgenre_id
+  );
 
-INSERT INTO subgenres (id, name)
+INSERT INTO subgenres (id, name);
 
-INSERT INTO authors (id, name)
+INSERT INTO authors (id, name);
 
-INSERT INTO books (id, title, year, series_id)
+INSERT INTO books (id, title, year, series_id);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id)
+INSERT INTO characters (id, name, motto, species, author_id, series_id);
 
-INSERT INTO character_books (id, book_id, character_id)
+INSERT INTO character_books (id, book_id, character_id);
