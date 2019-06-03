0054491c80181c3e18a28bba5f71b5b3f376a8fa
michaelstsnow <michael.st.snow@gmail.com>
2019-06-03 01:40:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN series ON series.id=characters.series_id
   INNER JOIN books ON series.id=books.series_id
-  GROUP BY characters.name"
+  GROUP BY characters.name
+  ORDER BY COUNT(books.id)"
 end
